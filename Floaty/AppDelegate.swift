6ce417b8eaa584cfd018772a4f64b41dc2d8461b
James Zaghini <james@zaghini.com>
2018-05-20 15:37:47 +1000

    Initial Commit

---
+++ Floaty/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Floaty
+//
+//  Created by James Zaghini on 20/5/18.
+//  Copyright © 2018 James Zaghini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
