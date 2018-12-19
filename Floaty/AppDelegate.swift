63f5176b93785ef4c3772376ebc0fd02f44e1df7
James Zaghini <james@zaghini.com>
2018-12-19 16:16:23 +1000

    Quit application when window closed

--- Floaty/AppDelegate.swift
+++ Floaty/AppDelegate.swift
@@ -15,4 +15,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
 }
