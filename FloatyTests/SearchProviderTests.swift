b30e39cc237d777d725cdc35d5dea560f095b343
James Zaghini <james@zaghini.com>
2018-12-22 23:36:10 +1000

    Test active SearchProvider

---
+++ FloatyTests/SearchProviderTests.swift
@@ -0,0 +1,37 @@
+//
+//  SearchProviderTests.swift
+//  FloatyTests
+//
+//  Created by James Zaghini on 22/12/18.
+//  Copyright © 2018 James Zaghini. All rights reserved.
+//
+
+import XCTest
+import Observable
+@testable import Floaty
+
+class SearchProviderTests: XCTestCase {
+
+    var userDefaults: UserDefaults!
+    let userDefaultsSuiteName = "TestDefaults"
+
+    override func setUp() {
+        super.setUp()
+        UserDefaults().removePersistentDomain(forName: userDefaultsSuiteName)
+        userDefaults = UserDefaults(suiteName: userDefaultsSuiteName)
+    }
+
+    func testDefaultProviderReturnedIfNoneChosenByUser() {
+        let settings = Settings.load(defaults: userDefaults)
+        let activeProvider = Search.activeProvider(settings: settings)
+        XCTAssert(activeProvider.providerId == Search.defaultProvider.providerId)
+    }
+
+    func testNonDefaultProviderReturnedIfChangedByUser() {
+        var settings = Settings.load(defaults: userDefaults)
+        settings.searchProviderId = Google().providerId
+        let activeProvider = Search.activeProvider(settings: settings)
+        XCTAssert(activeProvider.providerId == settings.searchProviderId)
+    }
+
+}
