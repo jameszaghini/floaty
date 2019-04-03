18c357dc1b6dd631e88b73b22e5330b84789059b
James Zaghini <james@zaghini.com>
2019-04-03 14:27:46 +1000

    Logs for Travis failure

--- floaty/WebViewController.swift
+++ floaty/WebViewController.swift
@@ -206,13 +206,14 @@ class WebViewController: NSViewController, ToolbarDelegate, WKUIDelegate, Javasc
         }
         print("xxx: ", "cccc")
         let isDarkMode = NSAppearance.isDarkMode(UserDefaults.standard)
+        print("xxx: ", "dddd")
         var html: String = ErrorHandler.shared.wrap { try String(contentsOfFile: path) } ?? ""
-        print("xxx: dddd ", html)
+        print("xxx: eeee ", html)
         html = html.replacingOccurrences(of: "{{title}}", with: title)
         html = html.replacingOccurrences(of: "{{message}}", with: message)
         html = html.replacingOccurrences(of: "{{bg-rgb}}", with: isDarkMode ? "30" : "246")
         html = html.replacingOccurrences(of: "{{bg-rgb}}", with: isDarkMode ? "rgb(110, 110, 110);" : "")
-        print("xxx: ", "eeee")
+        print("xxx: ", "ffff")
         self.html = html
     }
 
